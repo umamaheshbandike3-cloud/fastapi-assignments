c49117d71f8ccb87aec45dc3f28150992266dd59
P.V.Chandra Sekhar <112941354+6304343421@users.noreply.github.com>
2026-03-23 22:37:59 +0530

    created initial fastapi day1 (#22)
    Co-authored-by: P.V. Chandra Sekhar <chandrasekhar6304@gmail.com>

---
+++ Students/PVChandraSekhar/Day1/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to FastAPI! im chandrasekhar"}
+
+@app.get("/hello")
+def say_hello():
+    return {"message": "Hello, World!"}
