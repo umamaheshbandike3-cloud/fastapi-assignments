3c453067d03f1fd2b031ab9b6a86e6445586df6d
Manam Bindhu <bindhumanam@gmail.com>
2026-03-24 20:32:21 +0530

    day2: added GET APIs (#57)

--- student/day-2/main.py
+++ student/day-2/main.py
@@ -2,17 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Day 1 APIs
-@app.get("/")
-def home():
-    return {"message": "Welcome"}
-
-@app.get("/hello")
-def hello():
-    return {"message": "Hello Bindhu"}
-
-
-# Day 2 APIs
 @app.get("/greet")
 def greet(name: str):
     return {"message": f"Hello {name}"}
