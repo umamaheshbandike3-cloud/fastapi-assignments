30f8ffa2ca0dc45e10a6e28f6d6e0575cb10c644
G Chandra Shekar <gchshekar78gmail.com>
2026-03-24 21:18:46 +0530

    day2 work

---
+++ Students/ChandraShekar/day2/main.py
@@ -0,0 +1,11 @@
+om fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/greet")  #Get greet API
+def greet(name: str):
+    return {"message": f"hello {name} ,Welcome to FastAPI"}
+
+@app.get("/add")   #Get add API
+def add(a: int, b : int):
+    return{"result": a+b}
