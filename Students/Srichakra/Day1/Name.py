a9dfad0d7de2a38eb4430df775f6f60a25c63f99
KattaSrichakra <kattasrichakra@gmail.com>
2026-03-24 19:20:48 +0530

    Day-1:Basic API created (#13)
    * Day-1:Basic API created
    
    * Day-2: get method
    
    * Day3: Post Method

---
+++ Students/Srichakra/Day1/Name.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "My first API"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello First API"}    
