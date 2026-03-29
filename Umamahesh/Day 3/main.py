fdb7d62bdc349e408bbaa39cf3d1b3059bd9e97a
umamaheshbandike3-cloud <umamaheshbandike3@gmail.com>
2026-03-29 16:54:45 +0530

    Day 4 and Day 5 Tasks Completedd

--- Umamahesh/Day 3/main.py
+++ Umamahesh/Day 3/main.py
@@ -15,8 +15,7 @@ def read_root():
     return {"message": "Welcome to Day 3 FastAPI"}
 
 # POST API
-@app.post("/user")
+@app.post("/user", status_code=201)
 def create_user(user: User):
-    return {
-        "message": f"User {user.name} is {user.age} years old"
-    }
+    # We can access data using user.name and user.age
+    return {"message": f"User {user.name} created successfully", "age": user.age}
